c97aaebfe8c5015d18b9b9211cbdf08688768000
0qy <linqianye333@gmail.com>
2021-03-22 15:09:48 +1300

    added node modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "https://qianyelin-backend.herokuapp.com",
+        contentTypes: [
+          "exhibitions",
+        ],
+        queryLimit: 1000,
+      },
+    },
+    
   ],
 }
